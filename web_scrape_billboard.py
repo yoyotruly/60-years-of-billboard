38bb3f943030c08de272619d773a32128f2c5888
Yoyotruly <yoyo.truly@gmail.com>
2019-01-20 23:06:34 -0500

    Combine title and lyrics spiders

--- web_scrape_billboard.py
+++ web_scrape_billboard.py
@@ -1,48 +1,40 @@
-import requests
 import scrapy
-from scrapy.http import TextResponse
 from scrapy.crawler import CrawlerProcess
 
-# Define spider for scraping song titles
-class TitleSpider(scrapy.Spider):
 
-    name = 'title_spider'
-    
-    def start_requests(self):
-        urls = ['https://www.billboard.com/charts/pop-songs/2019-01-12']
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-    
+class ChartSpider(scrapy.Spider):
+    name = 'chart_spider'
+    start_urls = ['https://www.billboard.com/charts/pop-songs']
+
     def parse(self, response):
-        res = response.css('.chart-number-one__title, .chart-list-item__title-text').css('::text')
-        titles = [t.strip() for t in res.extract()]
-        return titles
+        # scrape artists and titles
+        artist_top = response.css('.chart-number-one__artist a::text')
+        title_top = response.css('.chart-number-one__title::text')
+        artist_rest = response.css('.chart-list-item::attr(data-artist)')
+        title_rest = response.css('.chart-list-item::attr(data-title)')
+        chart = {
+            'artist': [artist_top.extract_first().strip()]
+                      + artist_rest.extract(),
+            'title': title_top.extract()
+                     + title_rest.extract()
+        }
+        yield chart
 
-# Define spider for scraping available lyrics avaiable on billboard.com
-class LyricsSpider(scrapy.Spider):
-    
-    name = 'lyrics_spider'
-    
-    def start_requests(self):
-        urls = ['https://www.billboard.com/charts/pop-songs/2019-01-12']
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse_links)
-    
-    def parse_links(self, response):
-        res = response.css('.chart-number-one__lyrics a , .chart-list-item__lyrics a').xpath('./@href')
-        links = res.extract()
-        for link in links:
-            yield response.follow(url=link, callback=self.parse_lyrics)
-    
+        # follow link of available lyrics
+        lyrics_urls = response.css('.chart-list-item__lyrics a::attr(href)').extract()
+        for url in lyrics_urls:
+            yield scrapy.Request(url, self.parse_lyrics)
+
     def parse_lyrics(self, response):
-        headline = response.css('.article__headline::text').extract_first()
-        lyrics = response.css('p:nth-child(1)::text').extract()
-        lyrics_dict[headline] = lyrics
-
-lyrics_dict = dict()
+        headline = response.css('.article__headline::text')
+        lyrics = response.css('p:nth-child(1)::text')
+        lyrics = {
+            'headline': headline.extract(),
+            'lyrics': lyrics.extract()[:-3]
+        }
+        yield lyrics
 
 # Start crawlers
 process = CrawlerProcess()
-process.crawl(TitleSpider)
-process.crawl(LyricsSpider)
+process.crawl(ChartSpider)
 process.start()
